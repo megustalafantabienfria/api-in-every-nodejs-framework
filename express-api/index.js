c4b3dfee6b4720389a855256d0ad0789d8e8b8fe
megustalafantabienfria <megustalafantabienfria@hotmail.com>
2019-05-30 17:34:08 -0500

    Dotenv files updated with no real values, just for an example

--- express-api/index.js
+++ express-api/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
 
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const port = process.env.PORT || process.env.DEFAULT_PORT;
+const port = process.env.PORT || 8080;
 const mongoHost = process.env.DB_HOST;
 const mongoUser = process.env.DB_USER;
 const mongoPass = process.env.DB_PASS;
@@ -12,7 +12,7 @@ const mongoURI = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}`;
 
 (async () => {
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true });
+    // await mongoose.connect(mongoURI, { useNewUrlParser: true });
     await app.listen(port);
 
     console.log(`Express-api listening on port ${port} and connected to Mongo host ${mongoURI}`);
